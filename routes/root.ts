4bb820de45a04d5f31c3f606cc98fa637419fbc7
Adarsh2325 <adarsh.vinod.3762@gmail.com>
2025-10-15 10:26:33 +0530

    Initial commit

---
+++ routes/root.ts
@@ -0,0 +1,18 @@
+import { FastifyInstance, FastifyPluginOptions } from 'fastify';
+import loginRoutes from '../login/routes';
+import userRoutes from '../user/routes';
+
+export default async function routes(
+  fastify: FastifyInstance,
+  opts: FastifyPluginOptions
+) {
+  fastify.get('/', async () => {
+    return {
+      message: 'Welcome to Shinr User Management API',
+      environment: process.env.NODE_ENV || 'development',
+    };
+  });
+
+  fastify.register(loginRoutes, { prefix: '/user' });
+  fastify.register(userRoutes, { prefix: '/user-profile' });
+}
