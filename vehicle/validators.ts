723a1ed9a3af0d0d9b5c6298ba5acacd22a2dbbe
Adarsh2325 <adarsh.vinod.3762@gmail.com>
2025-10-23 23:38:22 +0530

    few validation change

--- vehicle/validators.ts
+++ vehicle/validators.ts
@@ -3,9 +3,11 @@ import Joi from 'joi';
 export const carSearchValidate = {
   query: Joi.object({
     search: Joi.string()
+      .pattern(/^[A-Za-z0-9\s]+$/) 
       .min(3)
       .optional()
       .messages({
+        'string.pattern.base': 'Model search term can only contain letters, numbers, and spaces',
         'string.min': 'Model search term must be at least 3 characters long',
       }),
 
@@ -27,7 +29,7 @@ export const carSearchValidate = {
     limit: Joi.number().integer().min(1).optional().default(10),
 
 
-    sortOrder: Joi.string().valid('ASC', 'DESC').optional().default('ASC'),
+    sortOrder: Joi.string().valid('ASC', 'DESC','asc','desc').optional().default('ASC'),
   }),
 
   body: Joi.object({}),
