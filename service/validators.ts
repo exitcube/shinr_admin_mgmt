85e1335772ccecbd9ec32ee369b6b7dfbb394f90
Ra-zimm <razmrz666@gmail.com>
2025-11-05 16:38:24 +0530

    service adding api

---
+++ service/validators.ts
@@ -0,0 +1,19 @@
+import Joi from 'joi';
+
+export const addServicesValidator = {
+    body: Joi.object({
+        name: Joi.string().required().messages({
+            'string.empty': 'Name is required',
+            'string.pattern.base': 'Name must be a string',
+            'string.min': 'Name must be at least 3 characters long',
+            'string.max': 'Name must be less than 100 characters long',
+        }),
+        imageId: Joi.string().optional().messages({
+            'string.pattern.base': 'Image ID must be a string',
+        }),
+        targetValue: Joi.string().required().messages({
+            'string.empty': 'Target value is required',
+            'string.pattern.base': 'Target value must be a string',
+        }),
+    }),
+};
