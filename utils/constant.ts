6e8ee2ca6ceeb00b9669a11ca3a29276777c9011
Ra-zimm <razmrz666@gmail.com>
2025-11-25 23:50:11 +0530

    requested chnages

--- utils/constant.ts
+++ utils/constant.ts
@@ -19,26 +19,21 @@ export enum BannerTargetAudience{
 }
 
 export const BannerStatus={
-    ACTIVE:{ displayValue: 'ACTIVE', value: 'ACTIVE' },
-    DRAFT:{ displayValue: 'DRAFT', value: 'DRAFT' },
-    EXPIRED:{ displayValue: 'EXPIRED', value: 'EXPIRED' },
+    ACTIVE:{ displayValue: 'active', value: 'ACTIVE' },
+    DRAFT:{ displayValue: 'draft', value: 'DRAFT' },
+    EXPIRED:{ displayValue: 'expired', value: 'EXPIRED' },
 }
 
 export  const BannerReviewStatus={
-    APPROVED:{ displayValue: 'APPROVED', value: 'APPROVED' },
-    REJECTED:{ displayValue: 'REJECTED', value: 'REJECTED' },
-    PENDING:{ displayValue: 'PENDING', value: 'PENDING' },
+    APPROVED:{ displayValue: 'approved', value: 'APPROVED' },
+    REJECTED:{ displayValue: 'rejected', value: 'REJECTED' },
+    PENDING:{ displayValue: 'pending', value: 'PENDING' },
 }
 
-export const BannerAudienceSelection=
-{
-    NEW_USER:{ displayValue: 'NEW USER', value: 'NEW_USER' },
-    FIRST_CAR_WASH:{ displayValue: 'FIRST CAR WASH', value: 'FIRST_CAR_WASH' },
-    FIRST_ANY_SERVICE:{ displayValue: 'FIRT ANY SERVICE', value: 'FIRST_ANY_SERVICE' },
-}
+ 
 
 export const BannerCategory = {
-  FESTIVAL: { displayValue: 'FESTIVAL', value: 'FESTIVAL' },
-  COME_BACK_USER: { displayValue: 'COMEBACK USER', value: 'COME_BACK_USER' },
-  MIN_CARWASH: { displayValue: 'MIN CARWASH', value: 'MIN_CARWASH' }
+  FESTIVAL: { displayValue: 'festival', value: 'FESTIVAL' },
+  COME_BACK_USER: { displayValue: 'comeBackUser', value: 'COME_BACK_USER' },
+  MIN_CARWASH: { displayValue: 'minCarWash', value: 'MIN_CARWASH' }
 } 
