fec28b91c0afc6fc24cef8c3daa558b3babd0e1d
Adarsh2325 <adarsh.vinod.3762@gmail.com>
2025-12-27 16:30:28 +0530

    SingleRewardApi

--- rewards/routes.ts
+++ rewards/routes.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance, FastifyPluginOptions } from 'fastify';
 import controller from './handler';
+import { adminAuthValidationPreHandler } from '../utils/authValidation';
+
 
 export default async function rewardsRoutes(fastify: FastifyInstance, opts: FastifyPluginOptions) {
     const handler = controller(fastify, opts);
+    fastify.get('/getSingleReward/:id',{ preHandler: [adminAuthValidationPreHandler] },handler.singleRewardHandler);
 }
